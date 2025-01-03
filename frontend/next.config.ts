7193dcf885379aa25bde8bde632f11c7e837aac1
Pedro Chaves <pedro.chaves@olx.com>
2025-01-03 16:37:57 +0000

    removed placeholders from nextjs config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BACKEND_URL: process.env.BACKEND_URL || 'http://127.0.0.1:8000',
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "PLACEHOLDER_CLIENT_ID",
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "PLACEHOLDER_CLIENT_SECRET",
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "PLACEHOLDER_NEXTAUTH_SECRET",
   },
 };
 
